5acd66a45dd818fbde0ffa130d133e2b47fff00a
Hasan <systemry420@gmail.com>
2020-11-27 16:00:01 +0200

    add burger icon for mobile

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,4 +10,26 @@ $(document).ready(function(){
             $('nav').removeClass('sticky')
         }
     })
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+            window.location.hash = hash;
+        });
+        } 
+    });
+
+    $('.js-nav-icon').click(function(){
+        var nav = $('.js-main-nav');
+        nav.toggle(200);
+    })
 })
